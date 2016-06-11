8c5945018949bfbe68cb670a461bcf9035e37e65
Alexei Fedotov <alexei.fedotov@gmail.com>
2016-06-11 13:31:17 +0300

    updated credits

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,9 @@
 cd `dirname "$0"`
 set -e
 
-BUILDDIR=/usr/share/nginx/html
+SCRIPTDIR=`pwd -P`
+#BUILDDIR=/usr/share/nginx/html
+BUILDDIR="../client-build"
 PROJECTDIR="../client/js"
 
 
@@ -17,8 +19,9 @@ cd $PROJECTDIR
 perl -p -e "s'dir: .*'dir: \"$BUILDDIR\",'" build.js >build-mod.js
 
 echo "Building client with RequireJS"
-mkdir -p $BUILDDIR/js
-cp -r ../../shared `dirname "$BUILDDIR"`
+cd "$THISDIR"
+mkdir -p "$BUILDDIR"/js
+cp -r ../../shared "$BUILDDIR"/..
 ls /usr/share/nginx/html/js/../../shared/js/gametypes.js
 
 node ../../bin/r.js -o build-mod.js
@@ -27,14 +30,14 @@ echo "Removing unnecessary js files from the build directory"
 # find $BUILDDIR/js -type f \( -iname "game.js" -or -iname "home.js" -or -iname "log.js" -or -iname "require-jquery.js" -or -iname "modernizr.js" -or -iname "css3-mediaqueries.js" -or -iname "mapworker.js" -or -iname "detect.js" -or -iname "underscore.min.js" -or -iname "text.js" \) -delete
 
 echo "Removing sprites directory"
-rm -rf $BUILDDIR/sprites
+rm -rf "$BUILDDIR"/sprites
 
 echo "Removing config directory"
-rm -rf $BUILDDIR/config
+rm -rf "$BUILDDIR"/config
 
 echo "Moving build.txt to current dir"
-mv $BUILDDIR/build.txt .
+mv "$BUILDDIR"/build.txt .
 
 echo "Build complete"
 
-chown nginx:nginx -R /usr/share/nginx/html
+chown nginx:nginx -R "$BUILDDIR"
